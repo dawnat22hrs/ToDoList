eeff11d6d71ce8a4fcea6373025152fcbef57e80
flightoffancy-b <petuhov.ilja2015@yandex.ru>
2023-02-16 19:08:12 +0400

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const domElements = {
     newTask: document.getElementById('newTask'),
     addTask: document.getElementById('addTask'),
-    list: document.getElementById('list')
+    list: document.getElementById('list'),
+    containerList: document.getElementById('containerList')
 }
 
-const todos = [];
+let todos = [];
 
 //функция добавления задачи по клику
 domElements.addTask.onclick = () => {
@@ -27,7 +28,7 @@ function addTodo(text){
     };
 
     todos.push(task);
-    
+    saveTodosLS();
 }
 
 //функция удаления задачи
@@ -37,6 +38,62 @@ function deleteTodo (id, list){
             list.splice(idi, 1);
         }
     })
+
+    saveTodosLS()
+}
+
+//функция редактирования задачи
+function editTodo(id, list) {
+    list.forEach((task, idi) => {
+        if(task.id == id){
+            domElements.newTask.value = list[idi].text;
+        }
+    })
+}
+
+//Функция добавления в LS
+function saveTodosLS(){
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+//вывод из LS
+if (localStorage.getItem('todos')){
+    todos = JSON.parse(localStorage.getItem('todos'))
+    renderTodo(todos)
+}
+
+//функция вывода пользовательского окна
+function renderUserWindow(containerList){
+    let htmlCodeWindow = '';
+
+    containerList.forEach(() => {
+        const taskHTMLWindow = `
+        <div class="popup">
+            <div class="popup__container">
+
+                <div class="popup__text title">
+                    <span class="text">Вы уверены, что хотите удалить заметку?</span>
+                </div>
+    
+                <div class="popup__btn">
+    
+                    <button class="btn btn_size btn-Y">
+                        Да
+                    </button>
+    
+                    <button class="btn btn_size btn-N" >
+                        Нет
+                    </button>
+    
+                </div>
+            </div>
+        </div>
+        `;
+
+        htmlCodeWindow = htmlCodeWindow + taskHTMLWindow;
+    });
+
+    domElements.containerList.innerHTML = htmlCodeWindow;
 }
 
 //функция вывода задач в поле
@@ -45,28 +102,24 @@ function renderTodo(list){
 
     list.forEach((task) => {
 
-        const cls = task.done ? 'todo__task todo__task_complete' : 'todo__task';
+        const clsTask = task.done ? 'todo__task todo__task_complete' : 'todo__task';
         const clsIcon = task.done ? '.icon-edit_complete' : 'icon-edit'
         const checked = task.done ? 'checked' : '';
         const taskHTML = `
         
+            <div id = "${task.id}" class="${clsTask}">
 
-            <div id = "${task.id}" class="${cls}">
-
-                
                 <label class="todo__checkbox">
                     <input type="checkbox" ${checked}>
                     <div class="checkbox__visible todo__checkbox-div"></div>
-                    <div class="todo__task-text todo__checkbox-div">
+                    <span class="todo__task-text todo__checkbox-div">
                         ${task.text}
-                    </div>
+                    </span>
                 </label>
-                        
-                
-                    
+                           
                 <div class="todo__task-icon">
                     <i class="${clsIcon}"></i>
-                    <i class="icon-del"></i>
+                    <i class="icon-del popup-link"></i>
                 </div>
                     
             </div>
@@ -78,16 +131,23 @@ function renderTodo(list){
     domElements.list.innerHTML = htmlCode;
 }
 
-//отслеживание статуса чекбокса
+domElements.containerList.onclick = (event) => {
+    const target = event.target;
+}
+
+
+//отслеживание кликов
 domElements.list.onclick = (event) => {
     const target = event.target;
     const checkboxEl = target.classList.contains('todo__checkbox-div');
     const deleteEl = target.classList.contains('icon-del');
+    const editEl = target.classList.contains('icon-edit');
 
     if (checkboxEl) {
         const task = target.parentElement.parentElement;
         const taskId = task.getAttribute('id');
 
+        console.log(taskId)
         taskStatus(taskId, todos);
         renderTodo(todos);
     }
@@ -95,7 +155,16 @@ domElements.list.onclick = (event) => {
     if (deleteEl) {
         const task = target.parentElement.parentElement;
         const taskId = task.getAttribute('id');
+        
+        deleteTodo(taskId, todos)
+        renderTodo(todos);
+    }
 
+    if (editEl) {
+        const task = target.parentElement.parentElement;
+        const taskId = task.getAttribute('id');
+
+        editTodo(taskId, todos);
         deleteTodo(taskId, todos);
         renderTodo(todos);
     }
@@ -108,4 +177,6 @@ function taskStatus(id, list){
             task.done = !task.done
         }
     })
+
+    saveTodosLS();
 }
